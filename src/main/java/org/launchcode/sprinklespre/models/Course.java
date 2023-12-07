0e1ed1d459598393c3983632b3da7a350a66506f
Zach <amidstbooks@gmail.com>
2023-12-06 23:28:24 -0600

    Added Module class and made changes to Course class.

--- src/main/java/org/launchcode/sprinklespre/models/Course.java
+++ src/main/java/org/launchcode/sprinklespre/models/Course.java
@@ -1,7 +1,26 @@
 package org.launchcode.sprinklespre.models;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToMany;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 public class Course extends AbstractEntity{
+
+    @OneToMany
+    @JoinColumn(name = "course_id")
+    private List<Module> modules = new ArrayList<>();
+
+    public Course() {}
+
+    public List<Module> getModules() {
+        return modules;
+    }
+
+    public void setModules(List<Module> modules) {
+        this.modules = modules;
+    }
 }
