511a43ae811968d0c509ac221c5b7a27c81eacbe
Sukanya <sukanya.s1793@gmail.com>
2023-12-11 23:07:44 -0600

    login n register

--- src/main/java/org/launchcode/sprinklespre/models/Course.java
+++
@@ -1,7 +0,0 @@
-package org.launchcode.sprinklespre.models;
-
-import jakarta.persistence.Entity;
-
-@Entity
-public class Course extends AbstractEntity{
-}
--- src/main/java/org/launchcode/sprinklespre/models/data/TestRepository.java
+++
@@ -1,9 +0,0 @@
-package org.launchcode.sprinklespre.models.data;
-
-import org.launchcode.sprinklespre.models.HelloWorld;
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface TestRepository extends CrudRepository<HelloWorld, Integer> {
-}
