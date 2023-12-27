cd93330a05fd6fe7f84cdd54fcf582ab8b6e6921
Madeline <moon.madeline@gmail.com>
2023-12-27 14:00:24 -0600

    Add CourseRepository and begin ApiCourseController

--- src/main/java/org/launchcode/sprinklespre/controller/ApiCourseCountroller.java
+++ src/main/java/org/launchcode/sprinklespre/controller/ApiCourseCountroller.java
@@ -1,10 +1,18 @@
 package org.launchcode.sprinklespre.controller;
 
-//TODO: Set up @CrossOrigin to localhost path for React
-//TODO: @RestController
-//TODO: @RequestMapping to appropriate path in React
+import org.launchcode.sprinklespre.models.data.CourseRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@CrossOrigin(origins = "http://127.0.0.1:5174/", maxAge = 3600)
+@RestController
+@RequestMapping("/api/courses")
 public class ApiCourseCountroller {
-    //TODO: Autowire CourseRepository
+    @Autowired
+    private CourseRepository courseRepository;
+
     //TODO: Return all courses: new ResponseEntity contain List<Course> courses of all courses
     //TODO: Return specific course: new ResponseEntity getCourseByID with @PathVariable
         //TODO: If coureOptional.isPresent() {return new ResponseEntity<> courseOptional.get(), HttpStatus.OK);
