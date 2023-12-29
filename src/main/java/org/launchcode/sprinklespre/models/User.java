f3f4b990e1c237f831ff58cef251ed569847d6bb
Zach <amidstbooks@gmail.com>
2023-12-29 13:17:32 -0600

    Mapped Course to User class with @ManyToMany relationship.

--- src/main/java/org/launchcode/sprinklespre/models/User.java
+++ src/main/java/org/launchcode/sprinklespre/models/User.java
@@ -1,22 +1,32 @@
 package org.launchcode.sprinklespre.models;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.ManyToMany;
 import jakarta.validation.constraints.NotNull;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 public class User extends AbstractEntity{
 
     //TODO: Map Courses to User with Many to Many relationships
+    @ManyToMany(mappedBy ="users")
+    private List<Course> courses = new ArrayList<>();
+
     @NotNull
     private String username;
 
     @NotNull
     private String pwHash;
+
     private static final BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
+
     public User() {}
 
     public User(String username, String password) {
+        super();
         this.username = username;
         this.pwHash = encoder.encode(password);
     }
@@ -26,5 +36,13 @@ public class User extends AbstractEntity{
     }
     public boolean isMatchingPassword(String password) {
         return encoder.matches(password, pwHash);
+    }
+
+    public List<Course> getCourses() {
+        return courses;
+    }
+
+    public void setCourses(List<Course> courses) {
+        this.courses = courses;
     }
 }
