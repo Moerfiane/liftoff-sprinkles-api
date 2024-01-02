6ec6178c0a0fe72c24a0a75f111faf2d657b292b
Zach <amidstbooks@gmail.com>
2024-01-01 19:09:43 -0600

    Added @ManyToMany relationships to Module class with Tool and Ingredient.

--- src/main/java/org/launchcode/sprinklespre/models/Module.java
+++ src/main/java/org/launchcode/sprinklespre/models/Module.java
@@ -1,13 +1,23 @@
 package org.launchcode.sprinklespre.models;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.ManyToMany;
 import jakarta.persistence.ManyToOne;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 public class Module extends AbstractEntity{
 
     @ManyToOne
     private Course course;
+
+    @ManyToMany
+    private List<Tool> tools = new ArrayList<>();
+
+    @ManyToMany
+    private List<Ingredient> ingredients = new ArrayList<>();
 
     boolean isCompleted;
 
