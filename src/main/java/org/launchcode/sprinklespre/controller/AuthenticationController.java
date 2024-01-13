ea5cf009e235b6aa68c2c51294fd81790d3de76b
Sukanya <sukanya.s1793@gmail.com>
2024-01-13 06:56:32 -0600

    review and roles

--- src/main/java/org/launchcode/sprinklespre/controller/AuthenticationController.java
+++ src/main/java/org/launchcode/sprinklespre/controller/AuthenticationController.java
@@ -1,7 +1,8 @@
 package org.launchcode.sprinklespre.controller;
 
-import jakarta.mail.MessagingException;
-import jakarta.mail.internet.MimeMessage;
+//import jakarta.mail.MessagingException;
+//import jakarta.mail.internet.MimeMessage;
+
 import jakarta.servlet.http.HttpSession;
 import org.launchcode.sprinklespre.models.User;
 import org.launchcode.sprinklespre.models.data.UserRepository;
@@ -9,13 +10,10 @@ import org.launchcode.sprinklespre.models.dto.LoginFormDTO;
 import org.launchcode.sprinklespre.models.dto.RegisterFormDTO;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.mail.javamail.JavaMailSender;
-import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.web.bind.annotation.*;
 
-import java.io.UnsupportedEncodingException;
-import java.security.SecureRandom;
-import java.util.*;
+import java.util.Map;
+import java.util.Optional;
 
 @RestController
 @CrossOrigin(origins = "*", maxAge = 3600)
@@ -23,8 +21,8 @@ public class AuthenticationController {
     @Autowired
     UserRepository userRepository;
 
-    @Autowired
-    public  JavaMailSender mailSender;
+    //@Autowired
+    //public  JavaMailSender mailSender;
 
     private static final String userSessionKey = "user";
 
@@ -66,10 +64,12 @@ public class AuthenticationController {
             return ResponseEntity.ok(Map.of("success", false, "message", "Passwords do not match"));
         }
 
-        String randomCode = generateRandomVerificationCode(); // Generate verification code
-        User newUser = new User(registerFormDTO.getUsername(), registerFormDTO.getPassword(), randomCode, false);
+        //String randomCode = generateRandomVerificationCode(); // Generate verification code
+        User newUser = new User(registerFormDTO.getUsername(), registerFormDTO.getPassword());
         newUser.setRole(registerFormDTO.getRole());
         userRepository.save(newUser);
+
+
 
 //        try {
 //            sendVerificationEmail(newUser, "http://localhost:8080");
@@ -95,40 +95,13 @@ public class AuthenticationController {
 //        return "Account verified successfully";
 //    }
 
-    public void sendVerificationEmail(User user, String siteURL) throws MessagingException, UnsupportedEncodingException {
-        String toAddress = user.getUsername();
-        String fromAddress = "your-email@example.com";
-        String senderName = "Your Company Name";
-        String subject = "Please verify your registration";
-        String content = "Dear [[name]],<br>"
-                + "Please click the link below to verify your registration:<br>"
-                + "<h3><a href=\"[[URL]]\" target=\"_self\">VERIFY</a></h3>"
-                + "Thank you,<br>"
-                + "Your company name.";
-
-        MimeMessage message = mailSender.createMimeMessage();
-        MimeMessageHelper helper = new MimeMessageHelper(message);
-
-        helper.setFrom(fromAddress, senderName);
-        helper.setTo(toAddress);
-        helper.setSubject(subject);
-
-        content = content.replace("[[name]]", user.getUsername()); // Assuming getUsername() returns user's name
-        String verifyURL = siteURL + "/verify?code=" + user.getVerificationCode();
-
-        content = content.replace("[[URL]]", verifyURL);
-
-        helper.setText(content, true);
-
-        mailSender.send(message);
-    }
-
 
     @GetMapping("/login")
     public ResponseEntity<LoginFormDTO> displayLoginForm() {
         LoginFormDTO loginFormDTO = new LoginFormDTO();
         return ResponseEntity.ok(loginFormDTO);
     }
+
     @PostMapping("/login")
     public ResponseEntity<?> loginUser(@RequestBody LoginFormDTO loginFormDTO) {
 
@@ -145,24 +118,13 @@ public class AuthenticationController {
             return ResponseEntity.badRequest().body("Invalid password");
         }
 
-       return ResponseEntity.ok(Map.of("success", true, "message", "User registered successfully", "userId", theUser.getId(), "role", theUser.getRole()));
+        return ResponseEntity.ok(Map.of("success", true, "message", "User registered successfully", "userId", theUser.getId(), "role", theUser.getRole()));
     }
+
     @GetMapping("/logout")
     public ResponseEntity<?> logout() {
         //request.getSession().invalidate();
         return ResponseEntity.ok("Logout successful");
     }
 
-    public static String generateRandomVerificationCode() {
-        String characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
-        int length = 6;
-        Random random = new SecureRandom();
-        StringBuilder verificationCode = new StringBuilder(length);
-
-        for (int i = 0; i < length; i++) {
-            verificationCode.append(characters.charAt(random.nextInt(characters.length())));
-        }
-
-        return verificationCode.toString();
-    }
 }
--- src/main/java/org/launchcode/sprinklespre/models/User.java
+++ src/main/java/org/launchcode/sprinklespre/models/User.java
@@ -1,6 +1,5 @@
 package org.launchcode.sprinklespre.models;
 
-import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.OneToMany;
 import jakarta.validation.constraints.NotNull;
@@ -23,18 +22,15 @@ public class User extends AbstractEntity{
     @NotNull
     private String role;
 
-    @Column(name = "verification_code", length = 64)
-    private String verificationCode;
 
-    private boolean enabled = false;
+   // private boolean enabled = false;
 
     private static final BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
     public User() {}
 
-    public User(String username, String password, String verificationCode, Boolean enabled ) {
+    public User(String username, String password ) {
         this.username = username;
-        this.verificationCode = verificationCode;
-        this.enabled = enabled;
+
         this.pwHash = encoder.encode(password);
         this.role = "user";
     }
@@ -46,21 +42,6 @@ public class User extends AbstractEntity{
         return encoder.matches(password, pwHash);
     }
 
-    public boolean isEnabled() {
-        return enabled;
-    }
-
-    public void setEnabled(boolean enabled) {
-        this.enabled = enabled;
-    }
-
-    public String getVerificationCode() {
-        return verificationCode;
-    }
-
-    public void setVerificationCode(String verificationCode) {
-        this.verificationCode = verificationCode;
-    }
 
     public String getRole() {
         return role;
